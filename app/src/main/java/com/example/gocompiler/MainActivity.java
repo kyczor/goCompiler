da445d20f4e6a29d0cb4573631e12db19a802b7a
kycz <czaplakaro@gmail.com>
2019-10-22 19:59:56 +0200

    Send a POST httprequest with simple json to a local golang HTTPServer and write its response on screen.

--- app/src/main/java/com/example/gocompiler/MainActivity.java
+++ app/src/main/java/com/example/gocompiler/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.gocompiler;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    String strFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +39,50 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("StaticFieldLeak")
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                new AsyncTask<Integer, Void, Void>(){
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @SuppressLint("WrongThread")
+                    @Override
+                    protected Void doInBackground(Integer... params) {
+                        try {
+                            TextView tv = findViewById(R.id.textView);
+                            tv.setText(sendPostRequest());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.execute(1);
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String sendPostRequest() throws IOException {
+        URL url = new URL("http://192.168.0.13:8014");
+        HttpURLConnection client = (HttpURLConnection) url.openConnection();
+        client.setRequestMethod("POST");
+        client.setRequestProperty("Content-Type", "application/json; utf-8");
+        client.setRequestProperty("Accept", "application/json");
+        client.setDoOutput(true);
+        String jsonInputString = "{\"title\": \"Miss\",\"name\": \"Karolina\", \"numbers\": {\"age\": 23, \"foot\": 42}}";
+        try(OutputStream os = client.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+            return(response.toString());
+        }
     }
 
     @Override
